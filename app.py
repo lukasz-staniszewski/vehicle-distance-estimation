1994573cc9ec8d53e15b85d7999556c76808c384
Lukasz Staniszewski <lukaszstaniszewski10@gmail.com>
2024-05-07 09:53:18 +0200

    feat: add app

---
+++ app.py
@@ -0,0 +1,53 @@
+import os
+
+import streamlit as st
+from PIL import Image
+from ultralytics import YOLO
+
+MODEL_PATH = "/net/tscratch/people/plglukaszst/projects/vehicle-distance-estimation/experiments/detection/yolov8-kitti-detection/train5/weights/best.pt"
+model = YOLO(MODEL_PATH)
+
+
+def load_image(image_file):
+    img = Image.open(image_file)
+    return img
+
+
+def save_image(image, filename):
+    image.save(filename)
+
+
+def process_image(image_path):
+    results = model([image_path])
+    output_files = []
+    for i, result in enumerate(results):
+        output_filename = f"result_{i}.jpg"
+        result.save(filename=output_filename)
+        output_files.append(output_filename)
+    return output_files
+
+
+st.title("Vehicle Detection App")
+st.write("Upload an image and the YOLO model will detect vehicles in the image.")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg"])
+
+if uploaded_file is not None:
+    image = load_image(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.write("")
+    st.write("Detecting...")
+
+    file_path = f"temp_{uploaded_file.name}"
+    save_image(image, file_path)
+
+    output_files = process_image(file_path)
+    for file in output_files:
+        st.image(file, caption="Processed Image with Detected Vehicles.", use_column_width=True)
+
+        with open(file, "rb") as img_file:
+            btn = st.download_button(label="Download Image", data=img_file, file_name=file, mime="image/jpeg")
+
+    os.remove(file_path)
+    for file in output_files:
+        os.remove(file)
